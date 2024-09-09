503f28367de25ce656d1e7d733f63aa9e8fa20f2
Tyler Woodcook <twoodcook@gmail.com>
2024-09-09 14:11:27 -0400

    add nav to header

--- header.php
+++ header.php
@@ -18,3 +18,27 @@
   <!-- CSS --> 
   <link rel="stylesheet" href="/assets/styles.css">
 </head>
+
+<body id="body" class="theme-light">
+  <div class="page-content">
+    <div class="section header-section">
+        <div class="header-title-container">
+          <div>
+            <a href="/">
+                <img class="header-logo" src="lp-logo-sticker.png">
+            </a>
+          </div>
+          <div>
+            <p class="h1 header-title mb-0">Linepup</p>
+            <p class="mb-0">Designer &amp; Developer</p>
+          </div>
+        </div>
+        <div class="header-theme-container">
+          <div>
+            <button onclick="lightTheme();" id="light-theme">&#9728;&#65039;</button>
+          </div>
+          <div>
+            <button onclick="darkTheme();" id="dark-theme">&#127761;</button>
+          </div>
+        </div>
+    </div>
