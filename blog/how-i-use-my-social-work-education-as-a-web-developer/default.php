df8ea957312e10d6e4b0a4300d6a2ac2a4f51703
Tyler Woodcook <twoodcook@gmail.com>
2024-09-09 14:13:39 -0400

    remove nav (its in header.php now)

--- blog/how-i-use-my-social-work-education-as-a-web-developer/default.php
+++ blog/how-i-use-my-social-work-education-as-a-web-developer/default.php
@@ -3,29 +3,6 @@ $pageTitle = 'How I use my social work education as a web developer';
 include('../../header.php');
 ?>
 
-<body id="body" class="theme-light">
-  <div class="page-content">
-    <div class="section header-section">
-        <div class="header-title-container">
-          <div>
-            <a href="/">
-                <img class="header-logo" src="../../lp-logo-sticker.png">
-            </a>
-          </div>
-          <div>
-            <p class="h1 header-title mb-0">Linepup</p>
-            <p class="mb-0">Designer &amp; Developer</p>
-          </div>
-        </div>
-        <div class="header-theme-container">
-          <div>
-            <button onclick="lightTheme();" id="light-theme">&#9728;&#65039;</button>
-          </div>
-          <div>
-            <button onclick="darkTheme();" id="dark-theme">&#127761;</button>
-          </div>
-        </div>
-    </div>
     <div class="section">
       <h2>How I use my social work education as a web developer</h2>
       <p>
