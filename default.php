5c456551206dde944ade72a2acd5d57d265030e0
Tyler Woodcook <twoodcook@gmail.com>
2024-09-09 14:11:01 -0400

    Remove nav -> move to header.php

--- default.php
+++ default.php
@@ -3,29 +3,6 @@ $pageTitle = 'Linepup Design & Development';
 include('header.php');
 ?>
 
-<body id="body" class="theme-light">
-  <div class="page-content">
-    <div class="section header-section">
-        <div class="header-title-container">
-          <div>
-            <a href="/">
-                <img class="header-logo" src="lp-logo-sticker.png">
-            </a>
-          </div>
-          <div>
-            <p class="h1 header-title mb-0">Linepup</p>
-            <p class="mb-0">Designer &amp; Developer</p>
-          </div>
-        </div>
-        <div class="header-theme-container">
-          <div>
-            <button onclick="lightTheme();" id="light-theme">&#9728;&#65039;</button>
-          </div>
-          <div>
-            <button onclick="darkTheme();" id="dark-theme">&#127761;</button>
-          </div>
-        </div>
-    </div>
     <div class="section">
         <h1>Hey, I'm Tyler and Building websites is my dream job.</h1>
         <p>I'm a web and graphic designer from the East Coast of USA with over 5 years of professional experience. I love designing aesthetic and efficient UI/UX for websites and applications. I thrive on creativity and I love to learn new things.</p>
