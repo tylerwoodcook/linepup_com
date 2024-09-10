396f77f5245e687ecc829bb8689688848d9adde6
Tyler <twoodcook@gmail.com>
2024-09-10 18:37:56 -0400

    New blog post

--- blog/default.php
+++ blog/default.php
@@ -7,6 +7,7 @@ include('../header.php');
         <h1>Blog</h1>
         <ul>
             <li><a href="/blog/how-i-use-my-social-work-education-as-a-web-developer/">How I Use My Social Work Education as a Web Developer</a></li>
+            <li><a href="/blog/things-we-feel-like-we-own-but-dont/">Things we feel like we own but don't</a></li>
         </ul>
       <hr />
     </div>
