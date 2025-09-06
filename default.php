ef418f6debc3e8a6a0bc53bf5539d03ad0177c88
Tyler <twoodcook@gmail.com>
2025-09-06 17:07:44 -0400

    Homepage - copy edits

--- default.php
+++ default.php
@@ -5,57 +5,26 @@ include('header.php');
 
     <div class="section">
         <h1>Hey, I'm Tyler and Building websites is my dream job.</h1>
-        <p>I'm a web and graphic designer from the East Coast of USA with over 5 years of professional experience. I love designing aesthetic and efficient UI/UX for websites and applications. I thrive on creativity and I love to learn new things.</p>
+        <p>I'm a web developer from the East Coast of USA with over five years of professional experience. I love designing aesthetic and efficient UI/UX for websites and applications. I thrive on creativity and I love to learn new things.</p>
         <h2>How it started</h2>
-        <p>I’ve been a professional designer for over five years, and I genuinely love what I do. In 2016, I started my own part-time freelance design company, taking on any creative projects I could get my hands on. Over the next few years I build an impressive portfolio of clients, working toward taking my first full time position in design.</p>
+        <p>I’ve been a professional developer/designer for over five years and I love what I do. In 2016, I started my own part-time freelance graphic design company, taking on any creative projects I could get my hands on. Over the next few years I build an impressive portfolio of clients, working toward taking my first full time position in design.</p>
         <p>In 2019, I took my design skills full time with Ready Training Online, an online learning & LMS company. During this time, I built not only my skills as a designer, but also got the opportunity to design and develop a few websites from scratch. I quickly realized how much I love building websites and functional user interfaces.</p>
         <p>In 2020, I was hired to design websites for a globally reaching leadership development company, CorpU. With clients like Coca-Cola, Rite-aid, Aetna, Charles Schwab and other heavy hitters, I was held to the highest standards. One of my first projects was a complete website overhaul, which involved design, development and branding. The new website enabled CorpU’s Sales & Customer Support teams to close deals and keep our customers informed like never before.</p>
         <h2>How it's going</h2>
-        <p>As CorpU grew to reach new audiences globally, we caught the attention of the world’s largest marketplace for online courses, Udemy. In August 2021, CorpU was acquired by Udemy Business, the branch of Udemy dedicated to offering enterprise solutions for businesses around the world.</p>
-        <p>Today, I’m proud to be a UI Developer for such a renowned company with an honorable mission to share knowledge with the world.</p>
+        <p>As CorpU grew to reach new audiences globally, we caught the attention of the world’s largest marketplace for online courses, Udemy. In August 2021, CorpU was acquired by Udemy.</p>
+        <p>Today, I’m proud to be a UI Developer for Udemy, a renowned company with an honorable mission to share knowledge with the world.</p>
         <h2>Skills &amp; abilities</h2>
-
-        <div class="col-1 mb-40">
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-solid fa-desktop"></i></div>
-            <div class="skill-text">Website design & development</div>
-          </div>
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-solid fa-code"></i></div>
-            <div class="skill-text">HTML, CSS, JavaScript</div>
-          </div>
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-brands fa-wordpress-simple"></i></div>
-            <div class="skill-text">WordPress &amp; PHP</div>
-          </div>
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-solid fa-arrow-pointer"></i></div>
-            <div class="skill-text">UI/UX design</div>
-          </div>
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-solid fa-palette"></i></div>
-            <div class="skill-text">Adobe creative apps</div>
-          </div>
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-solid fa-swatchbook"></i></div>
-            <div class="skill-text">Branding &amp; style guides</div>
-          </div>
-
-          <div class="skill-container">
-            <div class="skill-icon"><i class="fa-solid fa-object-group"></i></div>
-            <div class="skill-text">Application design systems</div>
-          </div>
-
-        </div>
-
+        <ul>
+          <li>Website design & development</li>
+          <li>HTML, CSS, JavaScript</li>
+          <li>WordPress &amp; PHP</li>
+          <li>UI/UX design</li>
+          <li>Adobe creative apps</li>
+          <li>Branding &amp; style guides</li>
+          <li>Application design systems</li>
+        </ul>
         <h2>Personal life</h2>
-        <p>When I’m not working, I like to stay busy playing with my two french bulldogs (Leia and Keira), recording music, woodworking or doing other creative things.</p>
+        <p>When I’m not working, I like to stay busy playing with my two french bulldogs (Leia and Keira), playing ice hockey, recording music, or doing other creative things.</p>
       <hr />
     </div>
 
