69d968f10bd6e0f9948d34601b5135606d90135b
Tyler <twoodcook@gmail.com>
2025-09-11 19:11:12 -0400

    change `default.php` to `index.php`

--- header.php
+++ header.php
@@ -32,8 +32,8 @@
           </a>
       </div><!-- END header-title-container -->
       <div id="site-nav"">
-        <div><a class="nav-item" href="/fun-stuff/default.php">Fun Stuff</a></div>
-        <div><a class="nav-item" href="/music/default.php">Music</a></div>
+        <div><a class="nav-item" href="/fun-stuff">Fun Stuff</a></div>
+        <div><a class="nav-item" href="/music">Music</a></div>
         <div><a class="nav-item" href="https://creativemarket.com/LinepupDesignCo" target="_blank">Store</a></div>
         <div class="header-theme-container">
           <div>
