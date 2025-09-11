ec98ffa0d0ece52f32d81f1f612541cbb226b1cf
Tyler <twoodcook@gmail.com>
2025-09-11 19:01:45 -0400

    fun stuff - change icons

--- fun-stuff/default.php
+++ fun-stuff/default.php
@@ -8,12 +8,12 @@ include('../header.php');
     <p>Here's a collection of some fun/simple/useful projects that I've made.</p>
     <hr>
     <a href="../dice-roller/index.html">
-        <h2><i class="fa-solid fa-link"></i> Simple dice roller</h2>
+        <h2><i class="fa-solid fa-dice-d20"></i> Simple dice roller</h2>
     </a>
     <p>Super simple project with unlimited uses. Roll for damage, roll for initiative, or maybe even roll to see what movie you're going to watch tonight.
     </p>
     <a href="../dm-screen/index.html">
-        <h2><i class="fa-solid fa-link"></i> Dungeon Master's Screen</h2>
+        <h2><i class="fa-solid fa-hat-wizard"></i> Dungeon Master's Screen</h2>
     </a>
     <p>If you’ve ever played a tabletop RPG, you know how wonderfully chaotic things can get.
         As the game master, I’m always looking for ways to keep sessions running smoothly without losing that energy.
