ea5cca96f0ab2824a02e42fcd25c96e885bf0818
Tyler Woodcook <twoodcook@gmail.com>
2025-10-08 19:48:10 -0400

    Spacing, formatting, etc. (#11)

--- footer.php
+++ footer.php
@@ -7,6 +7,6 @@
   <div style="display: flex; flex-direction: row; gap: 16px;">
     <div><a target="_blank" href="https://www.linkedin.com/in/twoodcook/"><i class="footer-social-icon fa-brands fa-linkedin"></i></a></div>
     <div><a target="_blank" href="https://github.com/tylerwoodcook"><i class="footer-social-icon fa-brands fa-github"></i></a></div>
-
+    <div><a target="_blank" href="https://creativemarket.com/LinepupDesignCo"><i class="footer-social-icon fa-solid fa-basket-shopping"></i></a></div>
   </div>
 </div>
--- index.php
+++ index.php
@@ -4,26 +4,8 @@ include('header.php');
 ?>
 
     <div class="section">
-        <h1>Hey, I'm Tyler and Building websites is my dream job.</h1>
-        <p>I'm a web developer from the East Coast of USA with over five years of professional experience. I love designing aesthetic and efficient UI/UX for websites and applications. I thrive on creativity and I love to learn new things.</p>
-        <h2>How it started</h2>
-        <p>I’ve been a professional developer/designer for over five years and I love what I do. In 2016, I started my own part-time freelance graphic design company, taking on any creative projects I could get my hands on. Over the next few years I build an impressive portfolio of clients, working toward taking my first full time position in design.</p>
-        <p>In 2019, I took my design skills full time with Ready Training Online, an online learning & LMS company. During this time, I built not only my skills as a designer, but also got the opportunity to design and develop a few websites from scratch. I quickly realized how much I love building websites and functional user interfaces.</p>
-        <p>In 2020, I was hired to design websites for a globally reaching leadership development company, CorpU. With clients like Coca-Cola, Rite-aid, Aetna, Charles Schwab and other heavy hitters, I was held to the highest standards. One of my first projects was a complete website overhaul, which involved design, development and branding. The new website enabled CorpU’s Sales & Customer Support teams to close deals and keep our customers informed like never before.</p>
-        <h2>How it's going</h2>
-        <p>As CorpU grew to reach new audiences globally, we caught the attention of the world’s largest marketplace for online courses, Udemy. In August 2021, CorpU was acquired by Udemy.</p>
-        <p>Today, I’m proud to be a UI Developer for Udemy, a renowned company with an honorable mission to share knowledge with the world.</p>
-        <h2>Skills &amp; abilities</h2>
-        <ul style="margin-bottom: 24px;">
-          <li>Website design & development</li>
-          <li>HTML, CSS, JavaScript</li>
-          <li>WordPress &amp; PHP</li>
-          <li>UI/UX design</li>
-          <li>Adobe creative apps</li>
-          <li>Branding &amp; style guides</li>
-          <li>Application design systems</li>
-        </ul>
-        <h2>Personal life</h2>
+        <h1 style="text-align: center;">What's up, dog?</h1>
+        <p>My name's Tyler. Welcome! I'm a web developer from the East Coast of USA with over five years of professional experience. I love creating aesthetic and efficient UI/UX for websites and applications. I thrive on creativity and I love to learn new things.</p>
         <p>When I’m not working, I like to stay busy playing with my two french bulldogs (Leia and Keira), playing ice hockey, recording music, or doing other creative things.</p>
     </div>
 
