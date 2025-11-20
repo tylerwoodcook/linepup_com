c9322718639dfb093cca69b340e3cebfe751a92f
Tyler Woodcook <twoodcook@gmail.com>
2025-11-19 20:41:34 -0500

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     $root = $_SERVER['DOCUMENT_ROOT'];
     $faviconPath = $root . '/lp-logo-sticker.png';
   ?>
-  <link rel="icon" type="image/x-icon" href="<?php $faviconPath ?>" />
+  <link rel="icon" type="image/x-icon" href="<?php echo $faviconPath ?>" />
   <meta charset="utf-8" />
   <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
   <meta content="Linepup Design and Development" name="This is the homepage for Linepup" />
