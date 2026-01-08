fb0bccb43123ee9d1dcffe7816d3ae45f4222b40
Tyler <twoodcook@gmail.com>
2026-01-07 19:59:53 -0500

    Web: add color amp post

--- web/index.php
+++ web/index.php
@@ -8,26 +8,35 @@ include('../header.php');
     <p class="mb-40">Here's a collection of some fun/simple/useful projects that I've made.</p>
 
     <div class="post-container">
-    <h2><i class="fa-solid fa-dice-d20"></i> Dice Roller</h2>
-    <p>Super simple project with unlimited uses. Roll for damage, roll for initiative, or maybe even roll to see what movie you're going to watch tonight.
-    </p>
-    <p style="font-style: italic;">Featured on <a href="https://fmhy.net/gaming-tools#tabletop-tools" target="_blank">Free Media Heck Yeah!</a></p>
-    <a href="../dice/index.html">
-        <button class="btn">Check it out!</button>
-    </a>
+        <h2><i class="fa-solid fa-brush"></i> Color Amp</h2>
+        <p>A color tool to quickly find and save colors using hue, saturation and brightness knobs. Build a color palette in minutes—nay, seconds! More features coming soon.
+        </p>
+        <a href="../color-amp/index.html">
+            <button class="btn">Check it out!</button>
+        </a>
     </div>
 
     <div class="post-container">
-    <h2><i class="fa-solid fa-hat-wizard"></i> Dungeon Master's Screen</h2>
-    <p>If you’ve ever played a tabletop RPG, you know how wonderfully chaotic things can get.
-        As the game master, I’m always looking for ways to keep sessions running smoothly without losing that energy.
-        This page has become a simple but powerful tool for me—helping me stay organized and quickly reference rules during play.
-        It’s designed with D&D 5E in mind, along with a sprinkle of homebrew ideas I like to use at my table.
-    </p>
-    <p style="font-style: italic;">Featured on <a href="https://fmhy.net/educational#dungeons-dragons" target="_blank">Free Media Heck Yeah!</a></p>
-    <a href="../dm-screen/index.html">
-        <button class="btn">Check it out!</button>
-    </a>
+        <h2><i class="fa-solid fa-dice-d20"></i> Dice Roller</h2>
+        <p>Super simple project with unlimited uses. Roll for damage, roll for initiative, or maybe even roll to see what movie you're going to watch tonight.
+        </p>
+        <p style="font-style: italic;">Featured on <a href="https://fmhy.net/gaming-tools#tabletop-tools" target="_blank">Free Media Heck Yeah!</a></p>
+        <a href="../dice/index.html">
+            <button class="btn">Check it out!</button>
+        </a>
+    </div>
+
+    <div class="post-container">
+        <h2><i class="fa-solid fa-hat-wizard"></i> Dungeon Master's Screen</h2>
+        <p>If you’ve ever played a tabletop RPG, you know how wonderfully chaotic things can get.
+            As the game master, I’m always looking for ways to keep sessions running smoothly without losing that energy.
+            This page has become a simple but powerful tool for me—helping me stay organized and quickly reference rules during play.
+            It’s designed with D&D 5E in mind, along with a sprinkle of homebrew ideas I like to use at my table.
+        </p>
+        <p style="font-style: italic;">Featured on <a href="https://fmhy.net/educational#dungeons-dragons" target="_blank">Free Media Heck Yeah!</a></p>
+        <a href="../dm-screen/index.html">
+            <button class="btn">Check it out!</button>
+        </a>
     </div>
 
 </div>
