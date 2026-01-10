7121b3733d92f4d6e064a4e376b8dd2ee30b9d9d
Tyler Woodcook <twoodcook@gmail.com>
2026-01-09 20:05:58 -0500

    Jan 2026 various updates (#16)
    * make CSS variables dynamic & lots of cleanup
    
    * minor edit to homepage copy

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('header.php');
     <div class="section">
         <h1 style="text-align: center;">What's up, dog?</h1>
         <p>My name's Tyler. Welcome! I'm a web developer from the East Coast of USA with over five years of professional experience. I love creating aesthetic and efficient UI/UX for websites and applications. I thrive on creativity and I love to learn new things.</p>
-        <p>When I’m not working, I like to stay busy playing with my two french bulldogs (Leia and Keira), playing ice hockey, recording music, or doing other creative things.</p>
+        <p>When I’m not working, I like to stay busy playing with my two french bulldogs (Leia and Keira), playing hockey, recording music, or doing other creative things.</p>
     </div>
 
     <?php 
--- music/index.php
+++ music/index.php
@@ -4,57 +4,52 @@ include('../header.php');
 ?>
 
 <div class="section">
-    <h1 style="text-align: center;">Music</h1>
+    <h1>Music</h1>
     <p>Here I’m sharing some of my musical projects. Some I consider done; others may still be works in progress. I hope you enjoy!</p>
 
-    <!-- FOREST STRINGS -->
+
+    <!-- SWELLS -->
     <div class="post-container">
-        <h2>Forest Strings</h2>
+        <h2>Swells</h2>
 
-        <p style="margin-bottom: 40px;">Forest Strings is a simple love song, which borders on naive. It taps into a childlike faith that believes love can last forever. Call it foolish, call it bold.</p>
+        <p style="margin-bottom: 40px;">Swells is a song about ups and downs.</p>
 
-        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center; margin-bottom: 40px;">
+        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center;">
             <div>
                 <audio controls="">
-                    <source src="forest-strings-4-15-2025.mp3" type="audio/mpeg">Your browser does not support the audio tag.
+                    <source src="swells-1-5-26.mp3" type="audio/mpeg">Your browser does not support the audio tag.
                 </audio>
             </div>
             <div>
-                <a href="forest-strings-4-15-2025.mp3" download="">
+                <a href="swells-1-5-26.mp3" download="">
                     <div class="download-audio" title="Download Song">
                         <i class="fa-solid fa-cloud-arrow-down" style="font-size: 24px; color: white;" aria-hidden="true"></i>
                     </div>
                 </a>
             </div>
         </div>
+    </div>
 
-        <h3>Lyrics</h3>
-        <p><strong class="lyrics-section">VERSE 1</strong><br>
-            "All in" doesn't mean we've got it easy<br>
-            Gotta work with what we've got even when it's not a lot<br>
-            Call it foolish, call it bold to take a hand that I can't fold<br>
-            But if I lose it all will you be there to break my fall?
-        </p>
-        <p><strong class="lyrics-section">VERSE 2</strong><br>
-            For every time I say you’re beautiful, I think it 40 more<br>
-            Pulling strings inside my heart like you’re assembling a chord<br>
-            It might look a little different, might never be enough<br>
-            But everything I have belongs to you, including all my love
-        </p>
+    <!-- FOREST STRINGS -->
+    <div class="post-container">
+        <h2>Forest Strings</h2>
 
-        <p><strong class="lyrics-section">CHORUS 1</strong><br>
-            Because I'll always love you<br>
-        </p>
+        <p style="margin-bottom: 40px;">Forest Strings is a simple love song, which borders on naive.</p>
 
-        <p><strong class="lyrics-section">VERSE 3</strong><br>
-            Let it never be a question, it never is for me<br>
-            When I close my eyes and picture perfect, you’re the one I see
-        </p>
-
-        <p><strong class="lyrics-section">CHORUS 2</strong><br>
-            Because I'll always love you<br>
-            I said it and I meant it
-        </p>
+        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center;">
+            <div>
+                <audio controls="">
+                    <source src="forest-strings-1-9-26.mp3" type="audio/mpeg">Your browser does not support the audio tag.
+                </audio>
+            </div>
+            <div>
+                <a href="forest-strings-1-9-26.mp3" download="">
+                    <div class="download-audio" title="Download Song">
+                        <i class="fa-solid fa-cloud-arrow-down" style="font-size: 24px; color: white;" aria-hidden="true"></i>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 
 
@@ -62,9 +57,9 @@ include('../header.php');
     <div class="post-container">
         <h2>Far from the Tree</h2>
 
-        <p style="margin-bottom: 40px;">This track leans heavily on autumnal themes. Growing up in Upstate New York, autumn has always been sentimental to me, and I’ve always been fascinated by the juxtaposition of the most beautiful season giving way to a long, frigid winter.</p>
+        <p style="margin-bottom: 40px;">Life changes with the ill-fated trees.</p>
 
-        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center; margin-bottom: 40px;">
+        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center;">
             <div>
                 <audio controls="">
                     <source src="far-from-the-tree-2-24-2025.mp3" type="audio/mpeg">Your browser does not support the audio tag.
@@ -78,21 +73,6 @@ include('../header.php');
                 </a>
             </div>
         </div>
-
-        <h3>Lyrics</h3>
-        <p><strong class="lyrics-section">VERSE 1</strong><br>
-            How could I fall so far from the tree?<br>
-            Trying to hide but your roots go so deep<br>
-            You say you love me but I can’t believe<br>
-            One so great could have love for one so weak
-        </p>
-        <p><strong class="lyrics-section">VERSE 2</strong><br>
-            Though great the fall, I watched you watch me<br>
-            Unripe and bruised in the mid-autumn breeze<br>
-            Watch my life change with the ill-fated trees<br>
-            Dangerously close to the earliest freeze
-        </p>
-
     </div>
 
 
@@ -102,13 +82,10 @@ include('../header.php');
         <h2>Faults</h2>
 
         <p>
-            I first recorded Faults in 2021 after spending a long time away from recording and writing music. I remember getting in trouble for trying to record vocals while my wife was asleep in the next room over (my bad). For me, this song is about the challenges of growing up and embracing new responsibilities. 
-        </p>
-        <p style="margin-bottom: 40px;">
-            Musically, this song channels some very nostalgic artists from my past, including The Beach Boys, Relient K, Weezer, and The Strokes.
+            Faults is a song about the challenges of growing up and embracing new responsibilities.
         </p>
 
-        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center; margin-bottom: 40px;">
+        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center;">
             <div>
                 <audio controls="">
                     <source src="faults-11-25-2023.mp3" type="audio/mpeg">Your browser does not support the audio tag.
@@ -122,43 +99,6 @@ include('../header.php');
                 </a>
             </div>
         </div>
-
-        <h3>Lyrics</h3>
-        <p><strong class="lyrics-section">VERSE 1</strong><br>
-            Two days alone And I’ll take my time<br>
-            By the end of this weekend I’ll be a hero in your eyes<br>
-            I know I told you that I’m trying to be a better man<br>
-            I just need to get my hands on something that can reel me in
-        </p>
-
-        <p><strong class="lyrics-section">PRE CHORUS 1</strong><br>
-            So many things I know I should but don’t want to start<br>
-            I’m still trying to live the way that I did when we were still apart
-        </p>
-
-        <p><strong class="lyrics-section">CHORUS</strong><br>
-            I’m waiting on a wake up call (the day I’ll finally wake up)<br>
-            You’re waiting on me too
-        </p>
-
-        <p><strong class="lyrics-section">VERSE 2</strong><br>
-            In my mind<br>
-            There’s a tunnel that takes what I sense and makes it come alive<br>
-            The walls collapsed when I was a child<br>
-            But I didn’t know that road was closed ‘til I was 25
-        </p>
-
-        <p><strong class="lyrics-section">PRE CHORUS 2</strong><br>
-            How can I see things clear with a pair of selective eyes?<br>
-            I’m losing sight of what we need and I’m just wasting time
-        </p>
-
-        <p><strong class="lyrics-section">CHORUS</strong><br>
-            I’m waiting on a wake up call (the day I’ll finally wake up)<br>
-            You’re waiting on me too
-        </p>
-
-
     </div>
 
 
@@ -166,11 +106,9 @@ include('../header.php');
     <div class="post-container">
         <h2>Wretched</h2>
 
-        <p style="margin-bottom: 40px;">Conceptually, Wretched is a song about coming to terms with my inherent imperfections. It’s inspired by Romans chapter 7, in which the author, Paul, admits that he can't do the “good” he wants to do: “For I do not understand my own actions. For I do not do what I want, but I do the very thing I hate.”</p>
+        <p style="margin-bottom: 40px;">“For I do not understand my own actions. For I do not do what I want, but I do the very thing I hate.” Romans chapter 7.</p>
 
-        <p style="margin-bottom: 40px;">Musically, the song draws inspiration from some of my most formative influences, such as Blink 182, Alkaline Trio, and other punk/pop-punk bands of that era.</p>
-
-        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center; margin-bottom: 40px;">
+        <div style="display: flex; flex-wrap: wrap; width: 100%; gap: 24px; align-items: center;">
             <div>
                 <audio controls="">
                     <source src="wretched-2-19-2024.mp3" type="audio/mpeg">Your browser does not support the audio tag.
@@ -184,47 +122,6 @@ include('../header.php');
                 </a>
             </div>
         </div>
-
-        <h3>Lyrics</h3>
-        <p><strong class="lyrics-section">VERSE 1</strong><br>
-            So what then, should I say? I’m a law breaker, I’m a disgrace<br>
-            I’m acting in opposition. The next day reality kicks in.<br>
-            If you see the flaws within me, and the evidence looks damning, <br>
-            will justice condemn me?
-        </p>
-
-        <p><strong class="lyrics-section">CHORUS</strong><br>
-            ‘Cause I can’t do the good I want.<br>
-            And I can’t live up to the law.<br>
-            If everything I do is wrong,<br>
-            I’m wretched, dead in the flesh and<br>
-            who will rescue me?
-        </p>
-
-        <p><strong class="lyrics-section">VERSE 2</strong><br>
-            Impatient, complacent. Want Heaven, but I’m Hell bent.<br>
-            Contracted a sickness. A feeling that I can’t kick this.<br>
-            There’s a conflict within me. What I planned against what I’m doing.<br>
-            So show me the good within me.
-        </p>
-
-        <p><strong class="lyrics-section">BRIDGE 1</strong><br>
-            If effort is counted<br>
-            My life reflects my progress<br>
-            If I chase what I think will help<br>
-            The end’s only as good as the plans I made myself
-        </p>
-
-        <p><strong class="lyrics-section">BRIDGE 2</strong><br>
-            You’re life, you’re love<br>
-            It’s dark but I feel your power<br>
-            I’m dead I’m lost<br>
-            My blood it starts to flow again<br>
-            And I can see my home ahead<br>
-            You’re life, you’re love<br>
-            I’m dead and lost
-        </p>
-        
     </div>
 
 
